b94e43c2800b4c55d00aa3b205d5a84883e9f6bb
dadhikary2560 <d.adhikary2560@gmail.com>
2026-02-18 22:45:51 +0530

    JavaScript is dynamic | JavaScript is Case Sensitive

--- first.js
+++ first.js
@@ -1,10 +1,22 @@
 console.log("Hello! new javascript program");
 
-x=29;
+//--------------------------------------------------------
+//javaScript is Dynamic
+
+x=29;                   //x is a number
 console.log(x);
 console.log(typeof(x));
 
 
-x="Debabrat";
+x="Debabrat";           //x is a string, however x is declared once
 console.log(x);
 console.log(typeof(x));
+
+//--------------------------------------------------------
+//javaScript is case sensitive
+
+fullName= "Debabrat";
+fullname= "Debabrat Adhikary";
+
+console.log(fullName);
+console.log(fullname);
