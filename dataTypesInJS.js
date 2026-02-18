732527bea83f9aef2f8a58f29af3f034eaa2cd31
dadhikary2560 <d.adhikary2560@gmail.com>
2026-02-19 00:08:43 +0530

    different data-types in JS

---
+++ dataTypesInJS.js
@@ -0,0 +1,21 @@
+//Different datatypes in javaScript
+
+//Numbers
+let x=70;
+console.log("data type of x is: "+typeof(x)+" & value of x: "+x);
+
+//String
+let y="Debabrat Adhikary";
+console.log("data type of y is: "+typeof(y)+" & value of y: "+y);
+
+//Boolean
+let z=true;
+console.log("data type of z is: "+typeof(z)+" & value of z: "+z);
+
+//UnDefined
+let a;
+console.log("data type of a is: "+typeof(a)+" & value of a: "+a);
+
+//Null
+let b=null;
+console.log("data type of b is: "+typeof(b)+" & value of b: "+b);
