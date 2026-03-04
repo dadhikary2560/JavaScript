afbbe9ef98f49a37b04e3c4f5efab8dde33babfc
dadhikary2560 <d.adhikary2560@gmail.com>
2026-03-04 21:00:34 +0530

    JS from data-type to function beginning

--- JAVASCRIPT2/javascriptPractice001.js
+++ JAVASCRIPT2/javascriptPractice001.js
@@ -64,3 +64,78 @@ else
 let a=prompt("enter value");
 console.log("value received from prompt: "+a);
 console.log(a>10 && a<=18? "teen" : "invalid input");
+
+
+// ------------------------------------------------------------
+//loops
+for(let i=0;i<10;i++)
+    console.log("Debabrat");
+
+let i=10;
+while(i>0)
+{
+    console.log(i);
+    console.log("while loop");
+    i--;
+}
+
+
+// ------------------------------------------------------------
+//String operation
+let abd="small letters";
+
+console.log(abd.toUpperCase());     //converts to uppercase letters, but the original remains intact
+for(let i=0;i<=abd.length;i++)
+    console.log(abd.charAt(i));
+
+console.log(abd.slice(2,8));        //prints a section of the whole length of the main string
+
+
+
+// ------------------------------------------------------------
+//array
+
+let arr=[1,2,3,4,5];
+console.log(arr);
+
+for(let i=0;i<arr.length;i++)
+    console.log(arr[i]);
+
+//array methods
+arr.push(67);       //adds value at the end also increases array size
+arr.push("Debabrat");
+console.log(arr);
+
+arr.pop();          //deletes value from end
+console.log(arr);
+
+let b=arr.toString();   //converts to a string a need to be saved in a different variable
+console.log(b);
+
+arr.shift();        //deletes from the beginning
+console.log(arr);
+
+
+// ------------------------------------------------------------
+//functions
+
+function m1(a)
+{
+    let b=a;
+    let rem=0;
+    let sum=0;
+    while(b>0)
+    {
+        rem=b%10;
+        sum=sum*10+rem;
+        b=Math.floor(b/10);
+        console.log(sum);
+    }
+console.log(sum);
+if(sum===a)
+    console.log("number is palindrome");
+else
+    console.log("not palindrome");
+};
+
+m1(101);
